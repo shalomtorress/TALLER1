8a7a06eff70440d53ba827581979d444a4a499fa
shalomtorrex <141857921+shalomtorress@users.noreply.github.com>
2023-09-24 01:32:19 -0500

    Add files via upload

---
+++ punto 4.py
@@ -0,0 +1,6 @@
+n1 = float(input("ingrese el primer numero: "))
+n2 = float(input("ingrese el segundo numero: "))
+if n1 % n2 == 0:
+   print("el primer numero es multiplo del segundo")
+else:
+   print("el prmer numero no es multiplo del segundo")
